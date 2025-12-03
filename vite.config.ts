d74707bb1cb061754b2b85b7ea35a4fb525c502e
Arpit <arpitiwari38925@gmail.com>
2025-12-03 19:53:58 +0530

    Fix project structure and vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  //vn This must match your repository name exactly, with slashes
+  base: '/Gate-exam-simulator/', 
 })
